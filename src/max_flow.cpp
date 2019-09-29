77ae868fb826295271710e3abfce131e5c06c435
Ronald <ronald.drp11@gmail.com>
2019-09-29 11:08:12 -0300

    udpated gitignore and changed from python to c++

---
+++ src/max_flow.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+bool BFS(int **residual_graph, int src, int sink, int *parent)
+{
+}
+
+int main(){
+    int n_vertexes, n_edges;
+
+    cin >> n_vertexes >> n_edges;
+
+    int graph[n_vertexes][n_vertexes] = {0};
+
+    cout << graph;
+}
