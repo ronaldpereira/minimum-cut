c9f2a2b92526fc059ea4f503165839016aefbb6e
Ronald <ronald.drp11@gmail.com>
2019-09-29 17:02:48 -0300

    finished code

--- src/max_flow.cpp
+++ src/max_flow.cpp
@@ -3,21 +3,19 @@
 #include <queue>
 #include <vector>
 
-using namespace std;
-
-bool BFS(vector<vector<int>> residual_graph, int src, int target, vector<int> *parent, vector<bool> *source_reachable, int n_vertex)
+bool BFS(std::vector<std::vector<int>> residual_graph, int src, int target, std::vector<int> *parent, std::vector<bool> *source_reachable, int n_vertex)
 {
-    (*source_reachable) = vector<bool>(n_vertex);
-    queue<int> BFSQueue;
+    (*source_reachable) = std::vector<bool>(n_vertex);
+    std::queue<int> BFSQueue;
     BFSQueue.push(src);
     (*source_reachable)[src] = true;
-    (*parent)[src] = -1;
+    (*parent)[src] = src;
 
     while (!BFSQueue.empty())
     {
-        if ((*source_reachable)[target] == true)
+        if ((*source_reachable)[target])
         {
-            break;
+            return true;
         }
 
         int u = BFSQueue.front();
@@ -28,110 +26,108 @@ bool BFS(vector<vector<int>> residual_graph, int src, int target, vector<int> *p
             if ((*source_reachable)[v] == false && residual_graph[u][v] > 0)
             {
                 BFSQueue.push(v);
-                (*parent)[v] = u;
                 (*source_reachable)[v] = true;
+                (*parent)[v] = u;
             }
         }
     }
-    return ((*source_reachable)[target] == true);
+
+    return false;
 }
 
-int EdmondsKarp(vector<vector<int>> graph, int source, int target, vector<bool> *source_reachable, int n_vertex)
+int Edmonds_Karp(std::vector<std::vector<int>> graph, int source, int target, std::vector<bool> *source_reachable, int n_vertex, int min_cut)
 {
-    vector<vector<int>> residual_graph(n_vertex, vector<int>(n_vertex));
-
+    std::vector<std::vector<int>> residual_graph(n_vertex, std::vector<int>(n_vertex));
     residual_graph = graph;
 
-    vector<int> parent(n_vertex);
+    std::vector<int> parent(n_vertex);
 
     int max_flow = 0;
 
-    while (BFS(residual_graph, source, target, &parent, source_reachable, n_vertex))
+    while (max_flow < min_cut && BFS(residual_graph, source, target, &parent, source_reachable, n_vertex))
     {
-        int path_flow = INT_MAX;
+        int min_path_flow = INT_MAX;
 
         for (int v = target; v != source; v = parent[v])
         {
             int u = parent[v];
-            path_flow = min(path_flow, residual_graph[u][v]);
+            min_path_flow = std::min(min_path_flow, residual_graph[u][v]);
         }
 
         for (int v = target; v != source; v = parent[v])
         {
             int u = parent[v];
-            residual_graph[u][v] -= path_flow;
-            residual_graph[v][u] += path_flow;
+            residual_graph[u][v] -= min_path_flow;
+            residual_graph[v][u] += min_path_flow;
         }
-        max_flow += path_flow;
+        max_flow += min_path_flow;
     }
 
     return max_flow;
 }
 
-void print_output(vector<bool> S_set, int max_flow)
+void print_output(std::vector<bool> S_set, int min_cut)
 {
     int S_size = 0;
-    for (int i = 0; i < S_set.size(); i++)
+    for (unsigned int i = 0; i < S_set.size(); i++)
     {
         if (S_set[i] == true)
         {
             S_size++;
         }
     }
-    cout << S_size << endl;
+    std::cout << S_size << std::endl;
 
-    for (int i = 0; i < S_set.size(); i++)
+    for (unsigned int i = 0; i < S_set.size(); i++)
     {
         if (S_set[i] == true)
         {
-            cout << i << " ";
+            std::cout << i << " ";
         }
     }
-    cout << endl;
+    std::cout << std::endl;
 
-    cout << max_flow << endl;
+    std::cout << min_cut << std::endl;
 }
 
 int main()
 {
     // Fast reading from input
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
 
     int n_vertex, n_edges;
 
-    cin >> n_vertex >> n_edges;
+    std::cin >> n_vertex >> n_edges;
 
-    vector<vector<int>> graph(n_vertex, vector<int>(n_vertex));
-    vector<bool> source_reachable(n_vertex);
-    vector<bool> S_set(n_vertex);
+    std::vector<std::vector<int>> graph(n_vertex, std::vector<int>(n_vertex));
+    std::vector<bool> source_reachable(n_vertex);
+    std::vector<bool> S_set(n_vertex);
 
     for (int i = 0; i < n_edges; i++)
     {
         int source, target, weight;
-        cin >> source >> target >> weight;
+        std::cin >> source >> target >> weight;
 
         graph[source][target] = weight;
+        graph[target][source] = weight;
     }
 
-    int max_flow = 0;
+    int min_cut = INT_MAX;
     for (int i = 0; i < n_vertex; i++)
     {
-        for (int j = 0; j < n_vertex; j++)
+        for (int j = i + 1; j < n_vertex; j++)
         {
-            if (i != j)
+            int cur_flow = Edmonds_Karp(graph, i, j, &source_reachable, n_vertex, min_cut);
+            if (cur_flow < min_cut)
             {
-                int cur_flow = EdmondsKarp(graph, i, j, &source_reachable, n_vertex);
-                if (cur_flow > max_flow)
-                {
-                    S_set = source_reachable;
-                    max_flow = cur_flow;
-                }
+                S_set = source_reachable;
+                min_cut = cur_flow;
             }
         }
     }
 
-    print_output(S_set, max_flow);
+    print_output(S_set, min_cut);
 
     return 0;
 }
