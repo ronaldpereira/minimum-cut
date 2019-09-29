c1d2ef26b70658dcea362164bcda18ede485342b
Ronald <ronald.drp11@gmail.com>
2019-09-29 12:40:16 -0300

    updated compile and execute scripts

--- src/compilar.sh
+++ src/compilar.sh
@@ -1,1 +1,1 @@
-g++ -Wall -Wextra -Ofast max_flow.cpp -o max_flow.out
+g++ -std=c++11 -Wall -Wextra -Ofast max_flow.cpp -o max_flow.out
--- src/executar.sh
+++ src/executar.sh
@@ -1,1 +1,1 @@
-./max_flow.out < ./data/1.in > ../data/output1.txt
+./max_flow.out < $1 > $2
